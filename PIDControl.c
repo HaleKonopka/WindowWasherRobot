b7abf026e33322c86331117acd92af47aa266c56
Griffin Della Grotte <gdellagrotte@gmail.com>
2020-03-01 22:29:08 -0800

    fixed initialization bug in pid

--- PIDControl.c
+++ PIDControl.c
@@ -5,6 +5,9 @@ void pid_contruct(pid_control_t *pid, float p, float i, float d, float ff){
     pid->i = i;
     pid->d = d;
     pid->ff = ff;
+    pid->accum = 0;
+    pid->lastError = 0;
+    pid->lastTimeMillisec = 0;
 }
 
 /**
@@ -17,15 +20,21 @@ void pid_contruct(pid_control_t *pid, float p, float i, float d, float ff){
  */
 float pid_control_calculate(pid_control_t *pid, float setpoint, float process, long time){
     long deltaTime = time - pid->lastTimeMillisec;
+    if (pid->lastTimeMillisec == 0) deltaTime = 0;
     pid->lastTimeMillisec = time;
 
     float error = setpoint - process;
 
-    float deltaError = (pid->lastError - error) * 1000.0 / (float) deltaTime;
+    float deltaError = 0;
+    if (deltaTime != 0) deltaError = (pid->lastError - error) * 1000.0 / (float) deltaTime;
     pid->lastError = error;
 
     float accumError = error * deltaTime / 1000.0;
     pid->accum += accumError;
 
     return pid->p * error + pid->i * pid->accum + pid->d * deltaError + pid->ff;
+}
+
+void pid_control_clear_integrator(pid_control_t *pid){
+    pid->accum = 0;
 }
